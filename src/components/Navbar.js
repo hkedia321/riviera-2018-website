36d4a8ba985ee6cdd998810f0c0e775e878ec501
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-12-16 16:21:17 +0530

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import './Navbar.css'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link
+} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './../actions';
+import PropTypes from 'prop-types';
 class Navbar extends React.Component{
+  constructor(props) {
+        super(props);
+
+    }
+  handleNav = (place) =>{
+    this.context.router.history.push("/"+place);
+  }
   render(){
+
     return(
         <nav style={{color:this.props.fontColor}}>
-          <ul>
-            <li>Events</li>
-            <li>Schedule</li>
-            <li>FAQ</li>
-            <li>Contact</li>
-          </ul>
+          <div className="main-nav-div">
+            <span className="main-nav-item" onClick={()=>this.handleNav("events")}>Events</span>
+            <span className="main-nav-item" onClick={()=>this.handleNav("schedule")}>Schedule</span>
+            <span className="main-nav-item" onClick={()=>this.handleNav("proshows")}>Proshows</span>
+            <span className="main-nav-item" onClick={()=>this.handleNav("faq")}>FAQ</span>
+            <span className="main-nav-item" onClick={()=>this.handleNav("contact")}>Contact</span>
+          </div>
         </nav>
     )
   }
 }
+function mapStateToProps(state){
+    return {
 
-export default Navbar
+    };
+}
+
+Navbar.contextTypes = {
+  router: PropTypes.object
+}
+export default connect(mapStateToProps,actions)(Navbar);
