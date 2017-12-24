10d4eacd979de7fd5ac10faaf5bcecb519f7a54b
Rahul A. Krishna <rkbalu524@gmail.com>
2017-12-24 09:45:23 +0530

    Campus Ambassador rules updated

--- src/components/CampusAmbassadorPage.js
+++ src/components/CampusAmbassadorPage.js
@@ -21,7 +21,7 @@ class CampusAmbassadorPage extends React.Component{
                 <div className='table-container'>
                   <Table
                     selectable={false}
-                    style={{background:'#2D2D2D',width:'110%',margin:'20px'}}
+                    style={{background:'#2D2D2D',width:'95%',margin:'20px'}}
                     >
                     <TableBody displayRowCheckbox={false}>
                       <TableRow>
@@ -101,6 +101,31 @@ class CampusAmbassadorPage extends React.Component{
                         <TableRow style={{color:'#fff'}} className='table-row'><TableRowColumn></TableRowColumn><TableRowColumn></TableRowColumn> <TableRowColumn>Proshow passes for all 4 days</TableRowColumn></TableRow>
                     </TableBody>
                   </Table>
+                </div>
+
+                <div className='rules-container'>
+                  <h3 style={{color:'#000'}}>DUTIES OF CAMPUS AMBASSADOR</h3>
+                  <ol>
+                    <li>Campus ambassadors are to act as the link between the Riviera Team and the concerned college administration and facilitate student communication.</li>
+                    <li>Be updated with Riviera ’18 happenings.</li>
+                    <li>To publicize and promote Riviera’18 by all possible means.</li>
+                    <li>They are required to share posters and other happenings of Riviera’18 through social media networks.</li>
+                  </ol>
+                  <br/><br/>
+                  <h3 style={{color:'#000'}}>TERMS AND CONDITIONS</h3>
+                  <ol>
+                    <li>Only 1 campus ambassador per college is permitted.</li>
+                    <li>The final decision of allotting the zone and category rests with the Riviera’18 team.</li>
+                    <li>Any kind of indiscipline caused by the ambassador during or before the fest will lead to severe disciplinary actions.</li>
+                    <li>The travel ticket should be booked in the name of the ambassador to claim the refund. The travel fares, as mentioned, entail train tickets of the 2 nd class compartments to all respective destinations from Vellore. (Maximum amount is INR 500 one way for Zone 1, INR 750 one way for Zone 2 and INR 1000 one way for Zone 3 and twice the amount for two way fares.)</li>
+                    <li>The travel expenses will be redeemed on the fourth day of the fest on submission of the ticket.</li>
+                    <li>As a proof of registrations the student ambassador needs to maintain a record of the details of registered students, in a format described by the Riviera’18 team.</li>
+                    <li>Each individual external entry shall be counted as one registration. </li>
+                    <li>Ambassadors are entitled to 3-day accommodation in the VIT hostels. No refund shall be provided if outside accommodation is acquired.</li>
+                    <li>The fest entry fee of INR 200 shall be waived for all the Campus ambassadors.</li>
+                    <li>Any permissions needed to market the Riviera in their college shall be taken care by the Student Ambassador, given that the Riviera’18 Team shall provide whatever help is necessary from their side.</li>
+                    <li>All decisions regarding the Campus ambassadors lay solely in the hands of the Riviera&#39;18 team.</li>
+                  </ol>
                 </div>
             </Col>
           </Row>
